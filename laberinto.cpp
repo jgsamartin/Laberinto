38f378e464efbbab3357b8dd016b508307fe5a9b
jgsamartin <jgsamartin@gmail.com>
2021-02-15 21:01:36 +0100

    Funciones esAccesible y esFinal

--- laberinto.cpp
+++ laberinto.cpp
@@ -2,7 +2,11 @@
 # include <iostream>
 using namespace std;
 
-# define N 100
+# define N 100          // Tamaño máximo del laberinto
+# define roca '#'        // Símbolo de la roca
+# define inicio 'I'      // Carácter de inicio
+# define fin 'O'         // Carácter de fin
+# define camino 'C'      // Carácter para indicar si se ha pasado por ahí
 
 /*
     Función para leer el archivo txt 
@@ -13,12 +17,38 @@ void cargaLaberinto (char *labFile, char labArray[N][N]) {
 }
 
 /*
-    Función que crea resuelve el laberinto con hilos
+    Función que comprueba si la casilla seleccionada es o no accesible
+*/
+int esAccesible (char labArray[N][N], int x, int y, int n, int m) {
+    return labArray[x][y] != roca && labArray[x][y] != inicio && labArray[x][y] != camino && x >= 0 && y >= 0 && x < n && y < m;
+}
+
+/*
+    Función que comprueba si la casilla seleccionada es o no el final
+*/
+int esFinal (char labArray[N][N], int x, int y) {
+    return labArray[x][y] == fin;
+}
+
+/*
+    Se mueve desde la posición (x,y) a todas las casillas adyacentes posibles
+*/
+int mueve (int x, int y) {
+
+}
+
+/*
+    Función que resuelve el laberinto
 */
 int juega (char labArray[N][N]) {
-    int inicio[1][2] = {21, 0};
-    int fin[1][2] = {5, 99};
+    int inicio[2] = {21, 0};
+    int fin[2] = {5, 99};
 
+    int i = inicio[0];
+    int j = inicio[1];
+
+    // --- Desde la posición inicial, se mueve
+    mueve (i,j);
 }
 
 /* Main */
